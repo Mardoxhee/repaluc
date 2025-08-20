557464a1a17090c423a8064db33ab4d00e5d32dd
mluviki <fonarevadmin@FONAREVs-iMac.local>
2025-08-20 15:45:18 +0100

    fix fix

--- app/acces-justice/layout.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import MainLayout from "@/components/layouts/MainLayout";
-
-export default function ClientsLayout({ children }: { children: React.ReactNode }) {
-  return <MainLayout>{children}</MainLayout>;
-}
